f48c3ecde441efecb4c038312033fa46eb22bfb2
maydindev <maydin.dev@outlook.com>
2024-08-14 15:04:42 +0300

    Updated.

--- src/App.js
+++ src/App.js
@@ -14,33 +14,29 @@ export default function App() {
 
   return (
     <>
-      <button onClick={openModal}>Modalı aç</button>
-
-      <Modal isOpen={isOpen} onClose={closeModal}>
-        <h1 className='pb-2 text-lg font-bold'>Modal açık</h1>
-        <button onClick={closeModal}>Kapalı</button>
-      </Modal>
+      <button
+        className="border-2 border-black py-1 px-2"
+        onClick={openModal}
+      >
+        Modalı aç
+      </button>
+      {isOpen && (
+        <Modal isOpen={isOpen}>
+          <h1 className="pb-2 text-lg font-bold">Modal açık</h1>
+          <button onClick={closeModal}>Kapat</button>
+        </Modal>
+      )}
     </>
-  )
+  );
 }
 
-function Modal({ isOpen, onClose,children }) {
-
+function Modal({isOpen,children}) {
   const dialogRef = useRef(null);
-
   useEffect(() => {
     if (isOpen) {
-      dialogRef.current?.showModal();
-    } else {
-      dialogRef.current?.close();
+      dialogRef.current.showModal();
     }
   }, [isOpen]);
-
   // ref.showModal()
-  return (
-    <dialog ref={dialogRef} className='border-2 border-black p-4' onClose={onClose}>
-      {children}
-    </dialog>
-  );
-  
+  return <dialog ref={dialogRef} className='border-2 border-black p-4'>{children}</dialog>
 }
